ce8fba1b2dd7242fecd181f5ab3bb4d9c05ce5d5
KODJO PIERRE AKPATA <kodjo-pierre.akpata@atos.net>
2025-11-11 23:49:41 +0100

    intégration docker compose pour les 3 services

---
+++ backend/src/test/java/org/impact/church/ChurchApplicationTests.java
@@ -0,0 +1,13 @@
+package org.impact.church;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChurchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
